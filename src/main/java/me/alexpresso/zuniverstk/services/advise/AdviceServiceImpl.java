57a590fda78753b05d28ebdecdfa1a188f14a69e
AlexPresso <blond.alexandre@hotmail.com>
2021-10-26 23:46:00 +0200

    WIP AdviceServiceImpl

--- src/main/java/me/alexpresso/zuniverstk/services/advise/AdviceServiceImpl.java
+++ src/main/java/me/alexpresso/zuniverstk/services/advise/AdviceServiceImpl.java
@@ -1,9 +1,12 @@
 package me.alexpresso.zuniverstk.services.advise;
 
+import me.alexpresso.zuniverstk.classes.FusionState;
 import me.alexpresso.zuniverstk.repositories.FusionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
 
 @Service
 public class AdviceServiceImpl implements AdviceService {
@@ -23,7 +26,13 @@ public class AdviceServiceImpl implements AdviceService {
         logger.debug("Preparing to advise {}...", discordTag);
         logger.debug("Checking for doable fusions...");
 
-        final var fusions = this.fusionRepository.findAllFusionsWithUserInv(discordTag);
+        final var fusions = this.fusionRepository.findAllFusionsWithUserInv(discordTag).stream()
+            .map(f -> new FusionState(f).refreshState(true))
+            .collect(Collectors.toSet());
+
+        final var doables = fusions.stream()
+            .filter(s -> s.getDoability() >= 100)
+            .collect(Collectors.toSet());
 
         logger.debug("Done advising {}.", discordTag);
     }
