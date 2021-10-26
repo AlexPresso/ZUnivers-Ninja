1982cd1c9d390942eef98168b7115df320e60e07
AlexPresso <blond.alexandre@hotmail.com>
2021-10-26 23:05:30 +0200

    WIP AdviceServiceImpl

--- src/main/java/me/alexpresso/zuniverstk/repositories/FusionRepository.java
+++ src/main/java/me/alexpresso/zuniverstk/repositories/FusionRepository.java
@@ -2,6 +2,15 @@ package me.alexpresso.zuniverstk.repositories;
 
 import me.alexpresso.zuniverstk.domain.nodes.item.Fusion;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.data.neo4j.repository.query.Query;
+
+import java.util.Set;
 
 public interface FusionRepository extends Neo4jRepository<Fusion, Long> {
+    @Query(
+        "MATCH (i:Item)-[r:FUSION_INPUT|FUSION_RESULT]-(f:Fusion) " +
+        "OPTIONAL MATCH (u:User{discordUserName: $0})-[iv:INVENTORY_ITEM]->(i) " +
+        "RETURN i, r, f, u, iv"
+    )
+    Set<Fusion> findAllFusionsWithUserInv(String discordUserName);
 }
--- src/main/java/me/alexpresso/zuniverstk/services/advise/AdviceServiceImpl.java
+++ src/main/java/me/alexpresso/zuniverstk/services/advise/AdviceServiceImpl.java
@@ -1,11 +1,30 @@
 package me.alexpresso.zuniverstk.services.advise;
 
+import me.alexpresso.zuniverstk.repositories.FusionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdviceServiceImpl implements AdviceService {
-    @Override
-    public void adviseUser(String discordTag) {
 
+    private static final Logger logger = LoggerFactory.getLogger(AdviceServiceImpl.class);
+
+    private FusionRepository fusionRepository;
+
+
+    public AdviceServiceImpl(final FusionRepository fr) {
+        this.fusionRepository = fr;
+    }
+
+
+    @Override
+    public void adviseUser(final String discordTag) {
+        logger.debug("Preparing to advise {}...", discordTag);
+        logger.debug("Checking for doable fusions...");
+
+        final var fusions = this.fusionRepository.findAllFusionsWithUserInv(discordTag);
+
+        logger.debug("Done advising {}.", discordTag);
     }
 }
