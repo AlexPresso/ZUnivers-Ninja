4c03877a5e866839f201b5bc141bf186e43daaa1
AlexPresso <15526962+AlexPresso@users.noreply.github.com>
2021-10-26 21:29:43 +0200

    Continue base architecture

--- src/main/java/me/alexpresso/zuniverstk/domain/base/BaseGraphObject.java
+++ src/main/java/me/alexpresso/zuniverstk/domain/base/BaseGraphObject.java
@@ -1,13 +1,19 @@
 package me.alexpresso.zuniverstk.domain.base;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
-import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseGraphObject {
     @Id
-    @GeneratedValue(UUIDStringGenerator.class)
-    private String id;
+    @GeneratedValue
+    protected Long graphObjectId;
+    protected String id;
+
+    public Long getGraphObjectId() {
+        return this.graphObjectId;
+    }
 
     public String getId() {
         return this.id;
--- src/main/java/me/alexpresso/zuniverstk/domain/nodes/item/Fusion.java
+++ src/main/java/me/alexpresso/zuniverstk/domain/nodes/item/Fusion.java
@@ -1,5 +1,6 @@
 package me.alexpresso.zuniverstk.domain.nodes.item;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import me.alexpresso.zuniverstk.domain.base.BaseGraphObject;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Relationship;
@@ -8,16 +9,26 @@ import java.util.Set;
 
 @Node
 public class Fusion extends BaseGraphObject {
+    @JsonProperty("items")
     @Relationship(type = "FUSION_INPUT", direction = Relationship.Direction.INCOMING)
     private Set<Item> inputs;
+    @JsonProperty("item")
     @Relationship(type = "FUSION_RESULT", direction = Relationship.Direction.OUTGOING)
     private Item result;
 
     public Set<Item> getInputs() {
         return this.inputs;
     }
+    public Fusion setInputs(final Set<Item> inputs) {
+        this.inputs = inputs;
+        return this;
+    }
 
     public Item getResult() {
         return this.result;
+    }
+    public Fusion setResult(final Item item) {
+        this.result = item;
+        return this;
     }
 }
