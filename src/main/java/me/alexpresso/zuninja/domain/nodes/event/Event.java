2100d8e9990d660699636ee0cf1f9a3583564d87
AlexPresso <15526962+AlexPresso@users.noreply.github.com>
2021-11-01 17:37:21 +0100

    ZUevents handling (#3)
    * Add ZUEvents handling

---
+++ src/main/java/me/alexpresso/zuninja/domain/nodes/event/Event.java
@@ -0,0 +1,65 @@
+package me.alexpresso.zuninja.domain.nodes.event;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import me.alexpresso.zuninja.classes.projection.ActionElement;
+import me.alexpresso.zuninja.domain.base.BaseGraphObject;
+import org.springframework.data.neo4j.core.schema.Node;
+
+import java.time.LocalDateTime;
+
+@Node
+public class Event extends BaseGraphObject implements ActionElement {
+
+    private int balanceCost;
+    private String name;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime beginDate;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime endDate;
+
+    public int getBalanceCost() {
+        return balanceCost;
+    }
+
+    public Event setBalanceCost(int balanceCost) {
+        this.balanceCost = balanceCost;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Event setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public LocalDateTime getBeginDate() {
+        return beginDate;
+    }
+
+    public Event setBeginDate(LocalDateTime beginDate) {
+        this.beginDate = beginDate;
+        return this;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public Event setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return this.name;
+    }
+}
---
+++ src/main/java/me/alexpresso/zuninja/services/event/EventServiceImpl.java
@@ -0,0 +1,62 @@
+package me.alexpresso.zuninja.services.event;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import me.alexpresso.zuninja.domain.nodes.event.Event;
+import me.alexpresso.zuninja.domain.nodes.item.Pack;
+import me.alexpresso.zuninja.repositories.EventRepository;
+import me.alexpresso.zuninja.repositories.PackRepository;
+import me.alexpresso.zuninja.services.item.ItemService;
+import me.alexpresso.zuninja.services.request.RequestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Service
+public class EventServiceImpl implements EventService {
+
+    private final static Logger logger = LoggerFactory.getLogger(EventServiceImpl.class);
+
+    private final RequestService requestService;
+    private final EventRepository eventRepository;
+
+
+    public EventServiceImpl(final RequestService rs, final EventRepository er) {
+        this.requestService = rs;
+        this.eventRepository = er;
+    }
+
+
+    @Override
+    public List<Event> fetchEvents() throws IOException, InterruptedException {
+        return (List<Event>) this.requestService.request("/public/event", "GET", new TypeReference<List<Event>>() {});
+    }
+
+    @Override
+    public List<Event> getEvents() {
+        return this.eventRepository.findAll();
+    }
+
+    @Override
+    public void updateEvents() throws IOException, InterruptedException {
+        logger.info("Updating events...");
+
+        final var events = this.fetchEvents();
+        final var dbEvents = this.getEvents().stream()
+            .collect(Collectors.toMap(Event::getId, Function.identity()));
+
+        events.forEach(e -> dbEvents.put(e.getId(), dbEvents.getOrDefault(e.getId(), e)
+            .setName(e.getName())
+            .setBeginDate(e.getBeginDate())
+            .setEndDate(e.getEndDate())
+        ));
+
+        this.eventRepository.saveAll(dbEvents.values());
+
+        logger.info("Events updated.");
+    }
+}
