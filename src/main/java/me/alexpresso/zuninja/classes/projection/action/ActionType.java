3d010a5aea6e63d45bd1b68a9d4e3d5d5ffcb05c
AlexPresso <blond.alexandre@hotmail.com>
2023-09-11 00:18:27 +0200

    Merge actions when previous action is same

--- src/main/java/me/alexpresso/zuninja/classes/projection/action/ActionType.java
+++ src/main/java/me/alexpresso/zuninja/classes/projection/action/ActionType.java
@@ -1,7 +1,7 @@
 package me.alexpresso.zuninja.classes.projection.action;
 
 public enum ActionType {
-    RECYCLE("recyclage"),
+    RECYCLE("recyclage", true),
     CRAFT("création"),
     ENCHANT("enchantement"),
     FUSION("fusion"),
@@ -10,19 +10,28 @@ public enum ActionType {
     LUCKY_RAYOU("lrlien"),
     DAILY("journa"),
     WEEKLY("bonus"),
-    CONSTELLATION("constellation"),
+    CONSTELLATION("constellation", true),
     EVOLUTION("évolution");
 
     private final String command;
+    private final boolean combinable;
 
     ActionType() {
         this(null);
     }
     ActionType(final String command) {
+        this(command, false);
+    }
+    ActionType(final String command, final boolean combinable) {
         this.command = command;
+        this.combinable = combinable;
     }
 
     public String getCommand() {
         return this.command;
+    }
+
+    public boolean isCombinable() {
+        return this.combinable;
     }
 }
