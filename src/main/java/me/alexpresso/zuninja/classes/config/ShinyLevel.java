aa148bffb171dddbce1aca0ae20a03c43df10816
AlexPresso <blond.alexandre@hotmail.com>
2024-01-15 23:03:57 +0100

    Add displayName

--- src/main/java/me/alexpresso/zuninja/classes/config/ShinyLevel.java
+++ src/main/java/me/alexpresso/zuninja/classes/config/ShinyLevel.java
@@ -3,18 +3,20 @@ package me.alexpresso.zuninja.classes.config;
 import java.util.Arrays;
 
 public enum ShinyLevel {
-    NORMAL(0, ""),
-    GOLDEN(1, "+"),
-    CHROMA(2, "*"),
-    UNKNOWN(-1, "?");
+    NORMAL(0, "", "Normales"),
+    GOLDEN(1, "+", "Dorées"),
+    CHROMA(2, "*", "Chromas"),
+    UNKNOWN(-1, "?", "???");
 
 
     private final int value;
     private final String discriminator;
+    private final String displayName;
 
-    ShinyLevel(final int value, final String discriminator) {
+    ShinyLevel(final int value, final String discriminator, final String displayName) {
         this.value = value;
         this.discriminator = discriminator;
+        this.displayName = displayName;
     }
 
     public int getValue() {
@@ -23,6 +25,10 @@ public enum ShinyLevel {
 
     public String getDiscriminator() {
         return this.discriminator;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
     }
 
     public static ShinyLevel valueOf(final int shinyLevel) {
