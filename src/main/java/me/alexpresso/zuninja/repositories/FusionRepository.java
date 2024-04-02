ce3eb3c30d6f45b3940a3dea7856bae588a6fd55
Alex <15526962+AlexPresso@users.noreply.github.com>
2024-04-02 15:51:15 +0200

    Upgrade (#43)
    * Bump dependencies and force Neo4J 5 dialect
    
    * Refactor docker-compose.yml with env vars

--- src/main/java/me/alexpresso/zuninja/repositories/FusionRepository.java
+++ src/main/java/me/alexpresso/zuninja/repositories/FusionRepository.java
@@ -3,5 +3,5 @@ package me.alexpresso.zuninja.repositories;
 import me.alexpresso.zuninja.domain.nodes.item.Fusion;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
-public interface FusionRepository extends Neo4jRepository<Fusion, Long> {
+public interface FusionRepository extends Neo4jRepository<Fusion, String> {
 }
