bbd3d219f2771b9c2b2afcfddde6835f3005b462
Alex <15526962+AlexPresso@users.noreply.github.com>
2024-09-01 00:54:02 +0200

    Cleaning and stack friendly fixes (#45)
    * Cleaning and stack friendly fixes

--- src/main/java/me/alexpresso/zuninja/classes/cache/MemoryCache.java
+++ src/main/java/me/alexpresso/zuninja/classes/cache/MemoryCache.java
@@ -1,12 +1,13 @@
 package me.alexpresso.zuninja.classes.cache;
 
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryCache {
 
     private final static String DEFAULT_CACHE_KEY = "__default__";
-    private final Map<String, Map<CacheEntry, Object>> cache;
+    private final Map<String, EnumMap<CacheEntry, Object>> cache;
 
 
     public MemoryCache() {
@@ -15,15 +16,18 @@ public class MemoryCache {
 
 
     public Object getOrDefault(final String discordTag, final CacheEntry key, final Object defValue) {
-        return this.cache.getOrDefault(discordTag, Map.of()).getOrDefault(key, defValue);
+        return this.cache
+            .getOrDefault(discordTag, new EnumMap<>(CacheEntry.class))
+            .getOrDefault(key, defValue);
     }
     public Object getOrDefault(final CacheEntry key, final Object defValue) {
         return this.getOrDefault(DEFAULT_CACHE_KEY, key, defValue);
     }
 
     public void put(final String discordTag, final CacheEntry key, final Object value) {
-        this.cache.computeIfAbsent(discordTag, k -> new HashMap<>());
-        this.cache.get(discordTag).put(key, value);
+        this.cache
+            .computeIfAbsent(discordTag, k -> new EnumMap<>(CacheEntry.class))
+            .put(key, value);
     }
     public void put(final CacheEntry key, final Object value) {
         this.put(DEFAULT_CACHE_KEY, key, value);
